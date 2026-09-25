ad879ffba69059235e1f829ee11c60329c23c7ff
Avarade637 <345179329@tdsb.ca>
2026-09-25 11:51:29 -0400

    Alerts

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 window.alert("Yo, wassup! How has your day been");
 window.alert("That's cool. Do you have a sec");
 window.alert("Nice :) Here you are...");
+window.alert("Uhh....");
+window.alert("Running out of stuff to say...");
+window.alert("Alright revenge time ;)");
